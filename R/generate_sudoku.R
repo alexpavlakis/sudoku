a5d1bf82c1b622f17021a52bfba2c95a0ae9f9b8
alexpavlakis <alexpavlakis@gmail.com>
2019-09-08 12:48:52 -0400

    add generate_sudoku

--- R/generate_sudoku.R
+++ R/generate_sudoku.R
@@ -1,44 +1,19 @@
+#' Sudoko generator
+#'
+#' This function generates sudoku puzzles
+#' @param seed integer used to create reproducible randomly generated puzzles.  defaults to NULL.
+#' @export
+#' @examples
+#' print_sudoku(sudoku)
+#' solved_sudoku <- solve_sudoku(sudoku)
+#' print_sudoku(solved_sudoku)
+
 generate_sudoku <- function(seed = NULL) {
   mat <- matrix(NA, nrow = 9, ncol = 9)
-  if(!is.null(seed)) {
-    set.seed(seed)
-  }
+  if(!is.null(seed)) set.seed(seed)
   mat[sample(1:9, size = 1), sample(1:9, size = 1)] <- sample(1:9, size = 1)
   solve_sudoku(mat, shuffle = TRUE)
 }
 
-generate_puzzle <- function(empties = 56, seed = NULL, unique = FALSE) {
-  full_puzzle <- generate_sudoku(seed = seed)
-  
-  out <- full_puzzle
-  not_the_same <- TRUE
-  while(not_the_same) {
-    
-    out <- full_puzzle
-    # Random pattern symmetrical
-    fill <- c(rep(NA, empties), rep(0, 81-empties))
-    fill <- fill[sample(1:81)]
-    om <- matrix(fill, nrow = 9)
-    om[lower.tri(om)] <- t(om)[lower.tri(om)]
-    
-    for(i in 1:9) {
-      for(j in 1:9) {
-        if(is.na(om[i, j]))
-          out[i, j] <- NA
-      }
-    }
-    if(unique) {
-      not_the_same <- !identical(full_puzzle, solve_sudoku(out, shuffle = T))
-    } else {
-      not_the_same <- FALSE
-    }
-    print(not_the_same)
-  } 
-  out
-}
 
 
-
-#puz <- generate_puzzle(59, unique = T)
-#print_sudoku(puz)
-#solve_sudoku(puz)
